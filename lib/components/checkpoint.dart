64983980e5a0b3e33086e90347f51e3cb74e5ed4
karmar4 <karmar.barrett97@gmail.com>
2023-09-03 12:43:11 +1000

    add checkpoint collision and animation

--- lib/components/checkpoint.dart
+++ lib/components/checkpoint.dart
@@ -1,22 +1,53 @@
 import 'dart:async';
 
+import 'package:flame/collisions.dart';
 import 'package:flame/components.dart';
+import 'package:pixel_adventure/components/player.dart';
 import 'package:pixel_adventure/pixel_adventure.dart';
 
 class Checkpoint extends SpriteAnimationComponent
-    with HasGameRef<PixelAdventure> {
+    with HasGameRef<PixelAdventure>, CollisionCallbacks {
   Checkpoint({position, size}) : super(position: position, size: size);
 
+  bool reachedCheckpoint = false;
   @override
   FutureOr<void> onLoad() {
-    // TODO: implement onLoad
+    debugMode = true;
+    add(RectangleHitbox(
+      position: Vector2(18, 56),
+      size: Vector2(12, 8),
+      collisionType: CollisionType.passive,
+    ));
+
     animation = SpriteAnimation.fromFrameData(
-        game.images
-            .fromCache('Items/Checkpoints/Checkpoint/Checkpoint (No Flag).png'),
-        SpriteAnimationData.sequenced(
-          amount: 1,
-          stepTime: 1,
-          textureSize: Vector2.all(64),
-        ));
+      game.images
+          .fromCache('Items/Checkpoints/Checkpoint/Checkpoint (No Flag).png'),
+      SpriteAnimationData.sequenced(
+        amount: 1,
+        stepTime: 1,
+        textureSize: Vector2.all(64),
+      ),
+    );
+  }
+
+  @override
+  void onCollision(Set<Vector2> intersectionPoints, PositionComponent other) {
+    if (other is Player && !reachedCheckpoint) _reachedCheckpoint();
+
+    super.onCollision(intersectionPoints, other);
+  }
+
+  void _reachedCheckpoint() {
+    reachedCheckpoint = true;
+    animation = SpriteAnimation.fromFrameData(
+      game.images.fromCache(
+          'Items/Checkpoints/Checkpoint/Checkpoint (Flag Out) (64x64).png'),
+      SpriteAnimationData.sequenced(
+        amount: 26,
+        stepTime: 0.05,
+        textureSize: Vector2.all(64),
+        loop: false,
+      ),
+    );
   }
 }
